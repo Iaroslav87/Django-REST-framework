55f8cf3529f5044d2059ddb37cb9c49169e6596b
Iaroslav87 <91834197+Iaroslav87@users.noreply.github.com>
2022-09-18 05:30:42 -0700

    Lesson1 hw
    Lesson1 hw

---
+++ library/authors/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Author
+from .serializers import AuthorModelSerializer
+
+
+class AuthorModelViewSet(ModelViewSet):
+    """
+    Класс набора представлений для модели Author
+    """
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
